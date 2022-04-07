0f3e15bc36905e9ee834735f71ac2cb7d37521ef
Raimund Andree <r.andree@live.com>
2022-04-07 12:32:43 +0200

    Added support for CimInstances

--- DscBuildHelpers/Public/Get-DscSplattedResource.ps1
+++ DscBuildHelpers/Public/Get-DscSplattedResource.ps1
@@ -1,4 +1,5 @@
-function Global:Get-DscSplattedResource {
+function Get-DscSplattedResource
+{
     [CmdletBinding()]
     Param(
         [String]
@@ -14,22 +15,26 @@ function Global:Get-DscSplattedResource {
         $NoInvoke
     )
     # Remove Case Sensitivity of ordered Dictionary or Hashtables
-    $Properties = @{}+$Properties
+    $Properties = @{} + $Properties
 
     $stringBuilder = [System.Text.StringBuilder]::new()
     $null = $stringBuilder.AppendLine("Param([hashtable]`$Parameters)")
     $null = $stringBuilder.AppendLine()
-    $null = $stringBuilder.AppendLine(' if ($Parameters) {')
-    $null = $stringBuilder.AppendLine(' $($Parameters=@{}+$Parameters)')
-    $null = $stringBuilder.AppendLine(' }') 
-    $null = $stringBuilder.AppendLine(" $ResourceName $ExecutionName { ")
-    foreach($PropertyName in $Properties.keys) {
+
+    if ($ExecutionName) {
+        $null = $stringBuilder.AppendLine(" $ResourceName '$ExecutionName' {")
+    }
+    else {
+        $null = $stringBuilder.AppendLine(" $ResourceName {")
+    }
+
+    foreach ($PropertyName in $Properties.Keys) {
         $null = $stringBuilder.AppendLine("$PropertyName = `$(`$Parameters['$PropertyName'])")
     }
-    $null = $stringBuilder.AppendLine("}")
-    Write-Debug ("Generated Resource Block = {0}" -f $stringBuilder.ToString())
+    $null = $stringBuilder.AppendLine('}')
+    Write-Debug ('Generated Resource Block = {0}' -f $stringBuilder.ToString())
 
-    if($NoInvoke.IsPresent) {
+    if ($NoInvoke) {
         [scriptblock]::Create($stringBuilder.ToString())
     }
     else {
@@ -40,5 +45,5 @@ function Global:Get-DscSplattedResource {
         }
     }
 }
-Set-Alias -Name x -Value Get-DscSplattedResource -scope Global
-#Export-ModuleMember -Alias x
+
+Set-Alias -Name x -Value Get-DscSplattedResource -Scope Global
