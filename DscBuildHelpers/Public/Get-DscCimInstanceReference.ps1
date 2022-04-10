e2ed009e241a642246971268df8b0517787880c9
Raimund Andree <r.andree@live.com>
2022-04-10 23:42:23 +0200

    Added function 'Get-DscCimInstanceReference'

---
+++ DscBuildHelpers/Public/Get-DscCimInstanceReference.ps1
@@ -0,0 +1,26 @@
+function Get-DscCimInstanceReference {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ResourceName,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ParameterName,
+
+        [Parameter()]
+        [hashtable]
+        $Data
+    )
+
+    if ($Script:allDscResourcePropertiesTable) {
+        if ($allDscResourcePropertiesTable.ContainsKey("$($ResourceName)-$($ParameterName)")) {
+            $p = $allDscResourcePropertiesTable."$($ResourceName)-$($ParameterName)"
+            $typeConstraint = $p.TypeConstraint -replace '\[\]', ''
+            Get-DscSplattedResource -ResourceName $typeConstraint -Properties $Data -NoInvoke
+        }
+    }
+    else {
+        Write-Host "No DSC Resource Properties metadata was found, cannot translate CimInstance parameters. Call 'Initialize-DscResourceMetaInfo' first is this is needed."
+    }
+}
