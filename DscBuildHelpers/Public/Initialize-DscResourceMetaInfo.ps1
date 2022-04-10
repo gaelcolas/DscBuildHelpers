a0cdac764c1f73777aa2934bb1fc07fc7d4ec3b0
Raimund Andree <r.andree@live.com>
2022-04-10 23:41:22 +0200

    Added function 'Initialize-DscResourceMetaInfo'

---
+++ DscBuildHelpers/Public/Initialize-DscResourceMetaInfo.ps1
@@ -0,0 +1,56 @@
+function Initialize-DscResourceMetaInfo
+{
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $ModulePath,
+
+        [Parameter()]
+        [switch]
+        $ReturnAllProperties,
+
+        [Parameter()]
+        [switch]
+        $Force
+    )
+
+    if ($script:allDscResourcePropertiesTable -and -not $Force)
+    {
+        return
+    }
+
+    $allModules = Get-ModuleFromFolder -ModuleFolder $ModulePath
+    $allDscResource = Get-DscResourceFromModuleInFolder -ModuleFolder $ModulePath -Modules $allModules
+    $modulesWithDscResources = $allDscResource | Select-Object -ExpandProperty ModuleName -Unique
+    $modulesWithDscResources = $allModules | Where-Object Name -In $modulesWithDscResources
+
+    $script:allDscResourcePropertiesTable = @{}
+
+    $script:allDscResourceProperties = foreach ($dscResource in $allDscResource)
+    {
+        $cimProperties = if ($ReturnAllProperties)
+        {
+            Get-DscResourceProperty -ModuleInfo ($modulesWithDscResources | Where-Object Name -EQ $dscResource.ModuleName) -ResourceName $dscResource.Name
+        }
+        else
+        {
+            Get-DscResourceProperty -ModuleInfo ($modulesWithDscResources | Where-Object Name -EQ $dscResource.ModuleName) -ResourceName $dscResource.Name | Where-Object { $_.TypeConstraint -like 'MSFT_*' -and $_.TypeConstraint -ne 'MSFT_Credential' }
+        }
+
+        foreach ($cimProperty in $cimProperties)
+        {
+            [PSCustomObject]@{
+                Name           = $cimProperty.Name
+                TypeConstraint = $cimProperty.TypeConstraint
+                IsKey          = $cimProperty.IsKey
+                Mandatory      = $cimProperty.Mandatory
+                Values         = $cimProperty.Values
+                Range          = $cimProperty.Range
+                ModuleName     = $dscResource.ModuleName
+                ResourceName   = $dscResource.Name
+            }
+            $script:allDscResourcePropertiesTable."$($dscResource.Name)-$($cimProperty.Name)" = $cimProperty
+        }
+
+    }
+}
