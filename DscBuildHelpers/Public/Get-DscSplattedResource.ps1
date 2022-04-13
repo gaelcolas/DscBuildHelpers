2c6d178c9ed652897ff7fc20ebf52476c1da499f
Raimund Andree <r.andree@live.com>
2022-04-13 09:25:03 +0200

    Next version of supporting CIM properties

--- DscBuildHelpers/Public/Get-DscSplattedResource.ps1
+++ DscBuildHelpers/Public/Get-DscSplattedResource.ps1
@@ -1,5 +1,4 @@
-function Get-DscSplattedResource
-{
+function Get-DscSplattedResource {
     [CmdletBinding()]
     Param(
         [String]
@@ -29,7 +28,40 @@ function Get-DscSplattedResource
     }
 
     foreach ($PropertyName in $Properties.Keys) {
-        $null = $stringBuilder.AppendLine("$PropertyName = `$(`$Parameters['$PropertyName'])")
+
+        $cimType = $allDscResourcePropertiesTable."$ResourceName-$PropertyName"
+        if ($cimType) {
+            $isCimArray = $cimType.TypeConstraint.EndsWith('[]')
+            $cimProperties = $Properties.$PropertyName
+            $null = $stringBuilder.AppendLine("$PropertyName = {0}" -f $(if ($isCimArray) { '@(' } else { "$($cimType.TypeConstraint.Replace('[]', '')) {" }))
+            if ($isCimArray) {
+                if ($Properties.$PropertyName -isnot [array]) {
+                    Write-Error "The property '$PropertyName' is an array and the BindingInfo data is not an array" -ErrorAction Stop
+                }
+                $i = 0
+                foreach ($cimPropertyValue in $cimProperties) {
+                    $null = $stringBuilder.AppendLine($cimType.TypeConstraint.Replace('[]', ''))
+                    $null = $stringBuilder.AppendLine('{')
+
+                    foreach ($cimSubProperty in $cimPropertyValue.GetEnumerator()) {
+                        $null = $stringBuilder.AppendLine("$($cimSubProperty.Name) = `$(`$Parameters['$PropertyName'][$($i)]['$($cimSubProperty.Name)'])")
+
+                    }
+                    $null = $stringBuilder.AppendLine('}')
+                    $i++
+                }
+                $null = $stringBuilder.AppendLine('{0}' -f $(if ($isCimArray) { ')' }))
+            }
+            else {
+                foreach ($cimProperty in $cimProperties.GetEnumerator()) {
+                    $null = $stringBuilder.AppendLine("$($cimProperty.Name) = `$(`$Parameters['$PropertyName']['$($cimProperty.Name)'])")
+                }
+                $null = $stringBuilder.AppendLine('}')
+            }
+        }
+        else {
+            $null = $stringBuilder.AppendLine("$PropertyName = `$(`$Parameters['$PropertyName'])")
+        }
     }
     $null = $stringBuilder.AppendLine('}')
     Write-Debug ('Generated Resource Block = {0}' -f $stringBuilder.ToString())
@@ -40,7 +72,8 @@ function Get-DscSplattedResource
     else {
         if ($Properties) {
             [scriptblock]::Create($stringBuilder.ToString()).Invoke($Properties)
-        } else {
+        }
+        else {
             [scriptblock]::Create($stringBuilder.ToString()).Invoke()
         }
     }
