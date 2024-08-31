1162b7e9cb916567f4186f52d60b897c193a7312
Raimund Andree <r.andree@live.com>
2024-08-31 07:48:12 +0200

    Added DSC test resources

---
+++ output/RequiredModules/MofBased/0.1.0/DSCResources/MofBasedResource2/MofBasedResource2.psm1
@@ -0,0 +1,77 @@
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $Key,
+
+        [Parameter()]
+        [Microsoft.Management.Infrastructure.CimInstance]
+        $Object1,
+
+        [Parameter()]
+        [Microsoft.Management.Infrastructure.CimInstance[]]
+        $Object1Group,
+
+        [Parameter()]
+        [System.String]
+        [ValidateSet('Absent', 'Present')]
+        $Ensure = 'Present'
+    )
+
+    return @{}
+}
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $Key,
+
+        [Parameter()]
+        [Microsoft.Management.Infrastructure.CimInstance]
+        $Object1,
+
+        [Parameter()]
+        [Microsoft.Management.Infrastructure.CimInstance[]]
+        $Object1Group,
+
+        [Parameter()]
+        [System.String]
+        [ValidateSet('Absent', 'Present')]
+        $Ensure = 'Present'
+    )
+}
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $Key,
+
+        [Parameter()]
+        [Microsoft.Management.Infrastructure.CimInstance]
+        $Object1,
+
+        [Parameter()]
+        [Microsoft.Management.Infrastructure.CimInstance[]]
+        $Object1Group,
+
+        [Parameter()]
+        [System.String]
+        [ValidateSet('Absent', 'Present')]
+        $Ensure = 'Present'
+    )
+
+    return $true
+}
