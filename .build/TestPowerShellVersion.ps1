e72f9e531a3b4acd96e35d73cd870207c17ab7ee
Raimund Andree <r.andree@live.com>
2024-10-21 11:37:37 +0200

    Formatting

--- .build/TestPowerShellVersion.ps1
+++ .build/TestPowerShellVersion.ps1
@@ -1,13 +1,17 @@
 task TestPowerShell5 {
+
     if ($PSVersionTable.PSEdition -ne 'Desktop')
     {
         Write-Error 'The build script requires PowerShell 7+ to work.'
     }
+
 }
 
 task TestPowerShell7 {
+
     if ($PSVersionTable.PSEdition -ne 'Core')
     {
         Write-Error 'The build script requires PowerShell 7+ to work.'
     }
+
 }
